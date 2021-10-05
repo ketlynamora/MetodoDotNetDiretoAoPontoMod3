c07c125df5cb414755e57226938e32ee42c8793e
Ketlyn Amora <ketypricila70@gmail.com>
2021-10-05 12:25:03 -0300

    "Aplicando a camada Core e Infrastructure"

---
+++ DevFreela.Core/Exceptions/ProjectAlreadyStartedException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DevFreela.Core.Exceptions
+{
+    public class ProjectAlreadyStartedException : Exception
+    {
+        public ProjectAlreadyStartedException() : base("Project is already in Started status")
+        {
+
+        }
+
+    }
+}
---
+++ DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -0,0 +1,25 @@
+﻿using DevFreela.Core.Entities;
+using System.Collections.Generic;
+
+namespace DevFreela.Infrastructure.Persistence
+{
+    public class DevFreelaDbContext
+    {
+        public DevFreelaDbContext()
+        {
+            Projects = new List<Project>
+            {
+                new Project("Meu projeto ASPNET Core 1", "Minha Descrição de Projeto 1", 1, 1, 10000),
+                new Project("Meu projeto ASPNET Core 2", "Minha Descrição de Projeto 2", 1, 1, 10000),
+                new Project("Meu projeto ASPNET Core 2", "Minha Descrição de Projeto 2", 1, 1, 10000)
+
+            };
+
+        }
+
+        public List<Project> Projects { get; set; }
+        public List<User> Users { get; set; }
+        public List<Skill> Skills { get; set; }
+
+    }
+}
